384f9e7833d91b87a204bda6866411c0a51c318c
Karakean <karakean@users.noreply.github.com>
2024-05-13 20:38:53 +0200

    blocking and non-blocking point-to-point communication

--- 2-master-slave/point-to-point/mpi.c
+++ 2-master-slave/point-to-point/mpi.c
@@ -15,14 +15,14 @@ int is_prime(unsigned long int number) {
     return 1;
 }
 
-int main(int argc, char **argv) {
+int main(int argc,char **argv) {
 
     Args ins__args;
     parseArgs(&ins__args, &argc, argv);
-    long inputArgument = ins__args.arg;
+    long inputArgument = ins__args.arg; 
     struct timeval ins__tstart, ins__tstop;
 
-    MPI_Init(&argc, &argv);
+    MPI_Init(&argc,&argv);
     int myrank, nproc;
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
     MPI_Comm_size(MPI_COMM_WORLD, &nproc);
@@ -31,36 +31,29 @@ int main(int argc, char **argv) {
     int *sendcounts = malloc(nproc * sizeof(int));
     int *displs = malloc(nproc * sizeof(int));
 
-    MPI_Request *send_requests = malloc(nproc * sizeof(MPI_Request));
-    MPI_Request *recv_requests = malloc(nproc * sizeof(MPI_Request));
-    MPI_Status *statuses = malloc(nproc * sizeof(MPI_Status));
-
-    int sum = 0;
+    
     for (int i = 0; i < nproc; i++) {
+        int sum = 0; 
         sendcounts[i] = inputArgument / nproc;
         if (i == nproc - 1) {
-            sendcounts[i] += inputArgument % nproc;
+            sendcounts[i] += inputArgument % nproc; // Add possible remainder to the last process
         }
         displs[i] = sum;
         sum += sendcounts[i];
     }
 
-    unsigned long int *recvbuf;
+    unsigned long int *recvbuf = (unsigned long int*)malloc(sendcounts[myrank] * sizeof(unsigned long int));
 
     if (!myrank) { // root
-        numbers = (unsigned long int *)malloc(inputArgument * sizeof(unsigned long int));
+        numbers = (unsigned long int*)malloc(inputArgument * sizeof(unsigned long int));
         numgen(inputArgument, numbers);
         gettimeofday(&ins__tstart, NULL);
-
-        recvbuf = numbers;
-
+        memcpy(recvbuf, numbers, sendcounts[0] * sizeof(unsigned long int));
         for (int i = 1; i < nproc; i++) {
-            MPI_Isend(numbers + displs[i], sendcounts[i], MPI_UNSIGNED_LONG, i, 0, MPI_COMM_WORLD, &send_requests[i-1]);
+            MPI_Send(numbers + displs[i], sendcounts[i], MPI_UNSIGNED_LONG, i, 0, MPI_COMM_WORLD);
         }
     } else {
-        recvbuf = (unsigned long int *)malloc(sendcounts[myrank] * sizeof(unsigned long int));
-        MPI_Irecv(recvbuf, sendcounts[myrank], MPI_UNSIGNED_LONG, 0, 0, MPI_COMM_WORLD, &recv_requests[myrank-1]);
-        MPI_Wait(&recv_requests[myrank-1], MPI_STATUS_IGNORE);
+        MPI_Recv(recvbuf, sendcounts[myrank], MPI_UNSIGNED_LONG, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
 
     int primecount = 0;
@@ -70,43 +63,24 @@ int main(int argc, char **argv) {
         }
     }
 
-    int *all_prime_counts = NULL;
     if (myrank == 0) {
-        all_prime_counts = malloc(nproc * sizeof(int));
-    }
-
-    if (myrank == 0) {
-        all_prime_counts[0] = primecount;
+        int total_primes = primecount;
+        int other_prime_count;
         for (int i = 1; i < nproc; i++) {
-            MPI_Irecv(&all_prime_counts[i], 1, MPI_INT, i, 0, MPI_COMM_WORLD, &recv_requests[i-1]);
-        }
-    } else {
-        MPI_Isend(&primecount, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &send_requests[myrank-1]);
-    }
-
-    if (myrank == 0) {
-        MPI_Waitall(nproc - 1, recv_requests, statuses);
-    }
-
-    if (myrank == 0) {
-        int total_primes = 0;
-        for (int i = 0; i < nproc; i++) {
-            total_primes += all_prime_counts[i];
+            MPI_Recv(&other_prime_count, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            total_primes += other_prime_count;
         }
         printf("Total number of primes for %ld elements: %d\n", inputArgument, total_primes);
         gettimeofday(&ins__tstop, NULL);
         ins__printtime(&ins__tstart, &ins__tstop, ins__args.marker);
         free(numbers);
-        free(all_prime_counts);
     } else {
-        free(recvbuf);
+        MPI_Send(&primecount, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
 
+    free(recvbuf);
     free(sendcounts);
     free(displs);
-    free(send_requests);
-    free(recv_requests);
-    free(statuses);
 
     MPI_Finalize();
     return 0;
